4d34644ea65a2e690c3e60cf3f7930404c8833d2
Raymond Wong <raymondwong1701@gmail.com>
2023-01-18 16:13:08 -0800

    Added getSquishmallowInfo()

--- src/squishfinder.js
+++ src/squishfinder.js
@@ -3,12 +3,13 @@ import { parse } from 'node-html-parser';
 
 const BASE_URL = 'https://squishmallowsquad.fandom.com'
 const MASTER_LIST_URL = `${BASE_URL}/wiki/Master_List`;
-const MASTER_LIST = [];
+export const MASTER_LIST = [];
 
-async function updateMasterList() {
+export async function updateMasterList() {
+    const old_count = MASTER_LIST.length;
     const response = await fetch(MASTER_LIST_URL);
     const html = parse(await response.text());
-    const container = findContainer();
+    const container = findContainer(html);
 
     if (!container) throw new Error(`Error! Container was not found for Squishmallow Master List on ${MASTER_LIST_URL}`);
     
@@ -36,24 +37,61 @@ async function updateMasterList() {
 
             const attributes = child.rawAttrs;
             if (attributes.includes('(page does not exist)')) continue;
-
-            const hrefIndex = attributes.indexOf('href="')+6;
-            const titleIndex = attributes.indexOf('title="')+7;
             MASTER_LIST.push({
-                name: attributes.substring(titleIndex, attributes.indexOf('"', titleIndex+1)),
-                link: BASE_URL + attributes.substring(hrefIndex, attributes.indexOf('"', hrefIndex+1))
+                name: child.getAttribute('title'),
+                link: BASE_URL + child.getAttribute('href')
             });
         }
     }
 
-    function findContainer() {
-        for (let div of html.getElementsByTagName("div")) {
-            if (div.classList.contains('mw-parser-output')) {
-                return div;
-            }
-        }
-        return null;
-    }
+    console.log(`Squishmallow Master List updated with ${MASTER_LIST.length} entries (${MASTER_LIST.length-old_count} added).`);
 }
 
-updateMasterList();
+export async function getSquishmallowInfo(squish) {
+    const URL = squish.link ? squish.link : squish;
+    const response = await fetch(URL.startsWith(BASE_URL) ? URL : BASE_URL + URL);
+    const html = parse(await response.text());
+    
+    const title = html.getElementById('firstHeading').rawText.trim();
+    const container = findContainer(html);
+
+    const aside = container.getElementsByTagName('aside')[0];
+    const img = aside.getElementsByTagName('img')[0].getAttribute('src');
+    
+    const map = new Map();
+    
+    map.set('title', title);
+    map.set('link', URL);
+    map.set('img', img);
+
+
+    let currentKey = 'subtext';
+    for (let child of container.childNodes) {
+        if (!child || !child.childNodes || !child.childNodes[0]) continue;
+        if (child.childNodes[0].classList?.contains('mw-headline')) {
+            currentKey = child.childNodes[0].rawText;
+            continue;
+        }
+        const content = child.rawText.trim();
+        if (currentKey && content && !content.includes('\t\t')) {
+            map.set(currentKey, content);
+            currentKey = '';
+        }
+    }
+
+    console.log(map);
+}
+
+function findContainer(html) {
+    for (let div of html.getElementsByTagName("div")) {
+        if (div.classList.contains('mw-parser-output')) {
+            return div;
+        }
+    }
+    return null;
+}
+
+updateMasterList().then(async () => {
+    const random = MASTER_LIST[Math.floor(Math.random() * MASTER_LIST.length)];
+    await getSquishmallowInfo(random);
+});
